037ea9c1e19afd548fc46dede676ede6dec6d7d5
jinalparikh25 <theparikh303@gmail.com>
2026-06-21 10:44:43 +0200

    Project setup and Database setup

---
+++ EmployeeManagement.API/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagement.API.Models
+{
+    public class Department
+    {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
